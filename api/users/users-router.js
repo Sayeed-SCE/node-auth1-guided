59f741be83c3262f0a4d73aaf50178d759ddbae1
James Blacklock <jamesblacklock@protonmail.com>
2022-07-25 13:27:05 -0400

    committing to "lecture"

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,6 +3,11 @@ const router = require("express").Router()
 const Users = require("./users-model.js")
 
 router.get("/", (req, res, next) => {
+  // if(userIsNotLoggedIn) {
+  //   next({ message: 'You must be logged in!' });
+  //   return;
+  // }
+
   Users.find()
     .then(users => {
       res.status(200).json(users)
